84444b92f8950685d7b92cd5a58226837b15d9b9
Katrina Wheelan <kwheelan@umich.edu>
2024-04-29 16:11:32 -0400

    changed static filepaths

--- routes.py
+++ routes.py
@@ -14,9 +14,9 @@ routes_bp = Blueprint('routes_bp', __name__)
 
 @routes_bp.route('/')
 def home():
-
     # Load the template and render with vars
     rendered_html = render_template('index.html', loading=True)  # Render the template
+    rendered_html = rendered_html.replace('"/static', '"static')  # Replace `"/static` with `"static`
     with open('index.html', 'w', encoding='utf-8') as file:  # Open a file to write
         file.write(rendered_html)  # Write the rendered HTML to the file
-    return rendered_html  # Return the rendered HTML to the client as usua
+    return rendered_html  # Return the rendered HTML to the client as usual
